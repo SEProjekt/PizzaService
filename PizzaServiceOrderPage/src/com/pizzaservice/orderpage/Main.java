d0e2f64aa37afb7fdb89d610d278cec98fc1bf78
Hoop77 <p.badenhoop@gmx.de>
2017-01-20 10:59:18 +0100

    ignoring out folder

--- PizzaServiceOrderPage/src/com/pizzaservice/orderpage/Main.java
+++ PizzaServiceOrderPage/src/com/pizzaservice/orderpage/Main.java
@@ -17,7 +17,7 @@ import java.io.IOException;
 public class Main extends Application
 {
     private static final String userName = "root";
-    private static final String password = "";
+    private static final String password = "m13u21m13PPILIHP";
     private static final String dbms = "mysql";
     private static final String serverName = "localhost";
     private static final String portNumber = "3306";
